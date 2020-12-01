cd209d4a2f36fe92ae6289029764ff8817f2fcd6
Prayson Wilfred Daniel <praysonpi@gmail.com>
2020-12-01 10:32:26 +0100

    Update linkedin_selenium.py

--- examples/web_automation/linkedin_selenium.py
+++ examples/web_automation/linkedin_selenium.py
@@ -82,7 +82,9 @@ class LinkedIn:
         
         # Remove the Automation Info 
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("--disable-infobars")
+        chrome_options.add_argument("start-maximized")
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_experimental_option('useAutomationExtension', False)
         driver = webdriver.Chrome(options=chrome_options)
         
         driver.get('https://www.linkedin.com/login')
